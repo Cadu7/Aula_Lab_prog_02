252f17a1bec417dede65f42aa14c7adb7e49387e
Cadu7 <cadumdecarvalho@gmail.com>
2021-11-03 21:13:03 -0300

    Aula 04 completa

---
+++ Aula_04_Recursividade/Aula_04.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int fatorial(int n){
+
+    if (n== 0 || n == 1){
+        return 1;
+    }else{
+        return n* fatorial(n-1);
+    } 
+    
+
+}
+
+int main(){
+    int n;
+    cout << "Digite um valor" << endl;
+    cin >> n;
+    cout << "Fatorial de " << n << " = " << fatorial(n) << endl;
+
+    return 0;
+}*/
+/*
+void imprimeIntIter(int a, int b, int c){
+
+    for (int i = a; i <= b; i+=c){
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+void imprimeIntRec(int a, int b, int c){
+
+    if(a <= b){
+        cout << a << " ";
+        imprimeIntRec(a+1,b,c);
+    }
+
+}    
+
+int main(){
+
+    imprimeIntIter(0,20,2);
+    imprimeIntRec(0,20,1);
+
+    return 0;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
